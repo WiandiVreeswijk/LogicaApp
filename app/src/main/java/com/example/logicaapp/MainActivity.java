af28008d9d5a45157cfe6a675209e53d016ece4b
Wiandi Vreeswijk <wiavre@gmail.com>
2019-02-08 14:15:19 +0100

    Initialize variables

--- app/src/main/java/com/example/logicaapp/MainActivity.java
+++ app/src/main/java/com/example/logicaapp/MainActivity.java
@@ -2,12 +2,23 @@ package com.example.logicaapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    private EditText mFirstAnswer;
+    private EditText mSecondAnswer;
+    private EditText mThirdAnswer;
+    private EditText mFourthAnswer;
+    private Button mSubmit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mFirstAnswer = findViewById(R.id.EditTextFirstAnswer);
+        mSecondAnswer = findViewById(R.id.EditTextSecondAnswer);
+        mThirdAnswer = findViewById(R.id.EditTextThirdAnswer);
+        mThirdAnswer = findViewById(R.id.EditTextFourthAnswer);
+        mSubmit = findViewById(R.id.buttonSubmit);
     }
 }
