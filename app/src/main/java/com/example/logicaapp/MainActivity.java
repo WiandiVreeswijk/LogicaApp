70e1e1bd2579dfa4d8fe24af52c0767de80af279
Wiandi Vreeswijk <wiavre@gmail.com>
2019-02-28 15:11:45 +0100

    When answers submitted give toaster message if answer is correct/incorrect

--- app/src/main/java/com/example/logicaapp/MainActivity.java
+++ app/src/main/java/com/example/logicaapp/MainActivity.java
@@ -2,15 +2,18 @@ package com.example.logicaapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private EditText mFirstAnswer;
     private EditText mSecondAnswer;
     private EditText mThirdAnswer;
     private EditText mFourthAnswer;
-    private Button mSubmit;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,7 +21,26 @@ public class MainActivity extends AppCompatActivity {
         mFirstAnswer = findViewById(R.id.EditTextFirstAnswer);
         mSecondAnswer = findViewById(R.id.EditTextSecondAnswer);
         mThirdAnswer = findViewById(R.id.EditTextThirdAnswer);
-        mThirdAnswer = findViewById(R.id.EditTextFourthAnswer);
-        mSubmit = findViewById(R.id.buttonSubmit);
+        mFourthAnswer = findViewById(R.id.EditTextFourthAnswer);
+        Button mSubmit = findViewById(R.id.buttonSubmit);
+
+        mSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                submitAnswers();
+            }
+        });
+    }
+    private void submitAnswers(){
+        String answer1 = mFirstAnswer.getText().toString();
+        String answer2 = mSecondAnswer.getText().toString();
+        String answer3 = mThirdAnswer.getText().toString();
+        String answer4 = mFourthAnswer.getText().toString();
+        if(answer1.equals("T") && answer2.equals("F") && answer3.equals("F") && answer4.equals("T")){
+            Toast.makeText(this, "Correct", Toast.LENGTH_SHORT).show();
+        }else{
+            Toast.makeText(this, "Incorrect", Toast.LENGTH_SHORT).show();
+        }
+
     }
 }
